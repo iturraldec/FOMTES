4d50e6a90aca89a884966048b34df019747dfd3c
iturraldec <iturraldec@gmail.com>
2025-08-15 11:56:08 -0400

    Modulo de Atención al Ciudadano, ok. [modulo-demo]

--- Modules/AtencionCiudadano/resources/views/index.blade.php
+++ Modules/AtencionCiudadano/resources/views/index.blade.php
@@ -1,5 +1,5 @@
 <x-atencionciudadano::layouts.master>
-    <h1>Hello World</h1>
+    <h1>Hello World!</h1>
 
     <p>Module: {!! config('atencionciudadano.name') !!}</p>
 </x-atencionciudadano::layouts.master>
