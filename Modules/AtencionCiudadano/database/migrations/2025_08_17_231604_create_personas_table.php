7804d6dbd6f82f98cfcf4e18c17f58c2a78c1f1b
iturraldec <iturraldec@gmail.com>
2025-08-17 20:29:36 -0400

    Creacion de tabla 'personas' [master]

---
+++ Modules/AtencionCiudadano/database/migrations/2025_08_17_231604_create_personas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('AtencionCiudadano.personas', function (Blueprint $table) {
+            $table->mediumIncrements('id');
+            $table->string('documento_id', 20)->unique();
+            $table->string('nombres', 100);
+            $table->string('apellidos', 100);
+            $table->string('email', 100)->unique()->nullable();
+            $table->integer('municipio_id')->unsigned();
+            $table->integer('parroquia_id')->unsigned();
+            $table->text('direccion');
+            $table->text('observaciones')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('municipio_id')->references('id_municipio')->on('public.municipios')->restrictOnDelete();
+            $table->foreign('parroquia_id')->references('id_parroquia')->on('public.parroquias')->restrictOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('AtencionCiudadano.personas');
+    }
+};
