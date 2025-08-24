0012e3ee33036c01a3b7f64c02cc9a54d5a4c9e7
iturraldec <iturraldec@gmail.com>
2025-08-23 21:15:10 -0400

    Tabla de visitas. [master]

---
+++ Modules/AtencionCiudadano/database/migrations/2025_08_24_004001_create_visitas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('AtencionCiudadano.visitas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->mediumInteger('persona_id');
+            $table->datetime('fecha');
+            $table->smallInteger('sitio_id');
+            $table->text('observaciones');
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('persona_id')->references('id')->on('AtencionCiudadano.personas')->restrictOnDelete()->onUpdate('cascade');
+            $table->foreign('sitio_id')->references('id')->on('AtencionCiudadano.sitios')->restrictOnDelete()->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('AtencionCiudadano.visitas');
+    }
+};
